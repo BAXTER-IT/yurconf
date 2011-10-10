3b3d4738c72eb65e7d546c3989f67554b82aabe4
ykryshchuk <>
2011-10-10 07:56:18 +0000

    QA-49. Minor improvements

--- src/main/cmd/template.cmd
+++ src/main/cmd/template.cmd
@@ -1,8 +1,15 @@
 
-BAXTER_CONFIG=http://localhost:8080/baxter-config/rest
-BAXTER_COMP=dbserver
+echo "Configuring PE Component from the Central Configuration Store..."
 
-JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configuration=%BAXTER_CONFIG%/log4j/%BAXTER_COMP%
-JAVA_OPTS=%JAVA_OPTS% -Dlogs.dir=c:\pelogs
-JAVA_OPTS=%JAVA_OPTS% -Dbaxter.config.url=%BAXTER_CONFIG%
-JAVA_OPTS=%JAVA_OPTS% -Dbaxter.component.id=%BAXTER_COMP%
+set BAXTER_CONFIG=http://localhost:8080/baxter-config/rest
+set BAXTER_COMP=%1
+
+set LOG4JXML=%BAXTER_CONFIG%/log4j/%BAXTER_COMP%/log4j.xml
+echo "Log4J XML: %LOG4JXML%"
+
+set JAVA_OPTS=%JAVA_OPTS% -Dlog4j.configuration=%LOG4JXML%
+set JAVA_OPTS=%JAVA_OPTS% -Dlogs.dir=c:\Baxter\pelogs
+set JAVA_OPTS=%JAVA_OPTS% -Dbaxter.config.url=%BAXTER_CONFIG%
+set JAVA_OPTS=%JAVA_OPTS% -Dbaxter.component.id=%BAXTER_COMP%
+
+echo "JAVA_OPTS: %JAVA_OPTS%"
