62d5c6209e78341fdea712b7730b5cd96e337cbc
ykryshchuk <>
2012-01-16 12:02:19 +0000

    CFG. Refactoring config server project structure. Added installation package for unix.

--- config-server/src/main/scripts/start-config-server.cmd
+++ config-server/src/main/scripts/start-config-server.cmd
@@ -3,6 +3,15 @@ rem
 rem Baxter Configuration Server start script
 rem v. ${project.version}
 
+set SCRIPT_DIR=%~dp0
+set JETTY_HOME=%SCRIPT_DIR%..
+set JAVA=java.exe
+
+pushd %JETTY_HOME%
+
 echo Starting Jetty Container...
+"%JAVA%" -jar start.jar
+
+popd
 
 endlocal
--- config-server/src/main/scripts/start-config-server.sh
+++ config-server/src/main/scripts/start-config-server.sh
@@ -2,6 +2,23 @@
 # Start script for Baxter Config Server
 # v. ${project.version}
 # 
-echo "Starting Jetty Container..."
 
+# Setup the JVM
+if [ "x$JAVA" = "x" ]; then
+    if [ "x$JAVA_HOME" != "x" ]; then
+        JAVA="$JAVA_HOME/bin/java"
+    else
+        JAVA="java"
+    fi
+fi
+
+SCRIPT_DIR="$(cd $(dirname $0);pwd)"
+JETTY_HOME=$SCRIPT_DIR/..
+
+pushd "$JETTY_HOME"
+
+echo "Starting Jetty Container..."
+"$JAVA" -jar start.jar
+
+popd
  
