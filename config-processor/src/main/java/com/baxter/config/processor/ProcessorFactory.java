94a0c9501ba456dc642906d218b42423c1087184
ykryshchuk <>
2012-12-03 15:17:19 +0000

    Failed build on XSD generating

--- config-processor/src/main/java/com/baxter/config/processor/ProcessorFactory.java
+++ config-processor/src/main/java/com/baxter/config/processor/ProcessorFactory.java
@@ -38,7 +38,8 @@ public class ProcessorFactory
   /**
    * Logger instance.
    */
-  private static final Logger LOGGER = LoggerFactory.getLogger(ProcessorFactory.class);
+  private static final Logger LOGGER = LoggerFactory
+      .getLogger(ProcessorFactory.class);
 
   /**
    * Name of the processor descriptor resource.
@@ -70,9 +71,10 @@ public class ProcessorFactory
    */
   private ProcessorFactory(final File repositoryRoot) throws ProcessorException
   {
-	LOGGER.trace("Creating factory with repository at {}", repositoryRoot.getAbsolutePath());
-	this.repository = Repository.getInstance(repositoryRoot);
-	loadProcessors();
+    LOGGER.trace("Creating factory with repository at {}",
+        repositoryRoot.getAbsolutePath());
+    this.repository = Repository.getInstance(repositoryRoot);
+    loadProcessors();
   }
 
   /**
@@ -84,9 +86,10 @@ public class ProcessorFactory
    * @throws ProcessorException
    *           if cannot get factory
    */
-  public static ProcessorFactory getInstance(final File repository) throws ProcessorException
+  public static ProcessorFactory getInstance(final File repository)
+      throws ProcessorException
   {
-	return new ProcessorFactory(repository);
+    return new ProcessorFactory(repository);
   }
 
   /**
@@ -101,29 +104,31 @@ public class ProcessorFactory
    * @throws ProcessorException
    *           if cannot match the processor for specified input
    */
-  public AbstractProcessor getProcessor(final ConfigID configId, final Version version) throws ProcessorException
+  public AbstractProcessor getProcessor(final ConfigID configId,
+      final Version version) throws ProcessorException
   {
-	final AbstractProcessor processor = this.processorsCache.findProcessor(configId);
-	if (processor == null)
-	{
-	  throw new ProcessorException("Unsupported configuration");
-	}
-	else
-	{
-	  if (processor.isVersionSupported(version))
-	  {
-		return processor;
-	  }
-	  else
-	  {
-		throw new ProcessorException("Unsupported version");
-	  }
-	}
+    final AbstractProcessor processor = this.processorsCache
+        .findProcessor(configId);
+    if (processor == null)
+    {
+      throw new ProcessorException("Unsupported configuration");
+    }
+    else
+    {
+      if (processor.isVersionSupported(version))
+      {
+        return processor;
+      }
+      else
+      {
+        throw new ProcessorException("Unsupported version");
+      }
+    }
   }
 
   public Repository getRepository()
   {
-	return this.repository;
+    return this.repository;
   }
 
   /**
@@ -131,75 +136,84 @@ public class ProcessorFactory
    */
   private void loadProcessors()
   {
-	try
-	{
-	  LOGGER.debug("Looking for available processors in classpath...");
-	  final Enumeration<URL> descriptorResources = Thread.currentThread().getContextClassLoader()
-		  .getResources(DESCRIPTOR_RESOURCE);
-	  if (!descriptorResources.hasMoreElements())
-	  {
-		LOGGER.warn("Could not find any processor in classpath");
-	  }
-	  while (descriptorResources.hasMoreElements())
-	  {
-		final URL descriptorResource = descriptorResources.nextElement();
-		try
-		{
-		  final Descriptor descriptor = Loader.getInstance().load(descriptorResource);
-		  LOGGER.debug("Found available processor - {}", descriptor);
+    try
+    {
+      LOGGER.debug("Looking for available processors in classpath...");
+      final Enumeration<URL> descriptorResources = Thread.currentThread()
+          .getContextClassLoader().getResources(DESCRIPTOR_RESOURCE);
+      if (!descriptorResources.hasMoreElements())
+      {
+        LOGGER.warn("Could not find any processor in classpath");
+      }
+      while (descriptorResources.hasMoreElements())
+      {
+        final URL descriptorResource = descriptorResources.nextElement();
+        try
+        {
+          final Descriptor descriptor = Loader.getInstance().load(
+              descriptorResource);
+          LOGGER.debug("Found available processor - {}", descriptor);
 
-		  try
-		  {
-			final Descriptor existingDescriptor = this.repository.getDescriptor(descriptor.getProductId());
-			// Now check if this descriptor is newer than the descriptor stored
-			// in the repository
-			// if this is newer one then apply update
-			final Version existingVersion = Version.valueOf(existingDescriptor.getVersion());
-			final Version availableVersion = Version.valueOf(descriptor.getVersion());
-			if (existingVersion.compareTo(availableVersion) < 0)
-			{
-			  final Upgrade upgrade = descriptor.getLatestUpgrade(existingDescriptor.getVersion());
-			  if (upgrade != null)
-			  {
-				this.repository.upgradePackage(descriptor, upgrade, this);
-			  }
-			  else
-			  {
-				LOGGER.warn("Cannot find upgrade from {} to {}", existingVersion, availableVersion);
-			  }
-			}
-			else
-			{
-			  LOGGER.debug("Processor in repository is up to date - {}", existingDescriptor);
-			}
-		  }
-		  catch (final RepositoryException e)
-		  {
-			// if there is no descriptor in repository then just copy entire
-			// package
-			LOGGER.info("Could not find {} in repository", descriptor);
-			this.repository.installPackage(descriptor);
-		  }
+          try
+          {
+            final Descriptor existingDescriptor = this.repository
+                .getDescriptor(descriptor.getProductId());
+            // Now check if this descriptor is newer than the descriptor stored
+            // in the repository
+            // if this is newer one then apply update
+            final Version existingVersion = Version.valueOf(existingDescriptor
+                .getVersion());
+            final Version availableVersion = Version.valueOf(descriptor
+                .getVersion());
+            if (existingVersion.compareTo(availableVersion) < 0)
+            {
+              final Upgrade upgrade = descriptor
+                  .getLatestUpgrade(existingDescriptor.getVersion());
+              if (upgrade != null)
+              {
+                this.repository.upgradePackage(descriptor, upgrade, this);
+              }
+              else
+              {
+                LOGGER.warn("Cannot find upgrade from {} to {}",
+                    existingVersion, availableVersion);
+              }
+            }
+            else
+            {
+              LOGGER.debug("Processor in repository is up to date - {}",
+                  existingDescriptor);
+            }
+          }
+          catch (final RepositoryException e)
+          {
+            // if there is no descriptor in repository then just copy entire
+            // package
+            LOGGER.info("Could not find {} in repository", descriptor);
+            this.repository.installPackage(descriptor);
+          }
 
-		  // Finally create processors and register them with cache
-		  for (Processor processorDescriptor : descriptor.getProcessors())
-		  {
-			final AbstractProcessor processor = createProcessor(descriptor, processorDescriptor);
-			this.processorsCache.registerProcessor(descriptor.getProductId(), processorDescriptor.getConfigurationType(),
-			    processor);
-		  }
+          // Finally create processors and register them with cache
+          for (final Processor processorDescriptor : descriptor.getProcessors())
+          {
+            final AbstractProcessor processor = createProcessor(descriptor,
+                processorDescriptor);
+            this.processorsCache.registerProcessor(descriptor.getProductId(),
+                processorDescriptor.getConfigurationType(), processor);
+          }
 
-		}
-		catch (final ProcessorException e)
-		{
-		  LOGGER.error("Could not load descriptor from " + descriptorResource, e);
-		}
-	  }
-	}
-	catch (final IOException e)
-	{
-	  LOGGER.error("Failed when looking for descriptors", e);
-	}
+        }
+        catch (final ProcessorException e)
+        {
+          LOGGER.error("Could not load descriptor from " + descriptorResource,
+              e);
+        }
+      }
+    }
+    catch (final IOException e)
+    {
+      LOGGER.error("Failed when looking for descriptors", e);
+    }
   }
 
   /**
@@ -213,49 +227,51 @@ public class ProcessorFactory
    * @throws ProcessorException
    *           if cannot create processor for any reason
    */
-  private AbstractProcessor createProcessor(final Descriptor descriptor, final Processor processorDescriptor)
-	  throws ProcessorException
+  private AbstractProcessor createProcessor(final Descriptor descriptor,
+      final Processor processorDescriptor) throws ProcessorException
   {
-	LOGGER.trace("Creating processor - {}", processorDescriptor);
-	try
-	{
-	  final Class<? extends AbstractProcessor> processorClass = Class.forName(processorDescriptor.getClassName()).asSubclass(
-		  AbstractProcessor.class);
-	  final Constructor<? extends AbstractProcessor> processorConstructor = processorClass.getConstructor(Descriptor.class,
-		  ProcessorFactory.class);
-	  final AbstractProcessor processor = processorConstructor.newInstance(descriptor, this);
-	  for (Parameter parameter : processorDescriptor.getParameters())
-	  {
-		LOGGER.trace("Setting {} in {}", parameter, processorDescriptor);
-		BUB.setProperty(processor, parameter.getName(), parameter.getValue());
-	  }
-	  return processor;
-	}
-	catch (final ClassNotFoundException e)
-	{
-	  LOGGER.error("Could not find processor class", e);
-	  throw new ProcessorException(e);
-	}
-	catch (final NoSuchMethodException e)
-	{
-	  LOGGER.error("Could not find constructor in processor class", e);
-	  throw new ProcessorException(e);
-	}
-	catch (final InvocationTargetException e)
-	{
-	  LOGGER.error("Processor constructor failed", e);
-	  throw new ProcessorException(e);
-	}
-	catch (final IllegalAccessException e)
-	{
-	  LOGGER.error("Processor constructor not accessible", e);
-	  throw new ProcessorException(e);
-	}
-	catch (final InstantiationException e)
-	{
-	  LOGGER.error("Could not instantiate processor", e);
-	  throw new ProcessorException(e);
-	}
+    LOGGER.trace("Creating processor - {}", processorDescriptor);
+    try
+    {
+      final Class<? extends AbstractProcessor> processorClass = Class.forName(
+          processorDescriptor.getClassName()).asSubclass(
+          AbstractProcessor.class);
+      final Constructor<? extends AbstractProcessor> processorConstructor = processorClass
+          .getConstructor(Descriptor.class, ProcessorFactory.class);
+      final AbstractProcessor processor = processorConstructor.newInstance(
+          descriptor, this);
+      for (final Parameter parameter : processorDescriptor.getParameters())
+      {
+        LOGGER.trace("Setting {} in {}", parameter, processorDescriptor);
+        BUB.setProperty(processor, parameter.getName(), parameter.getValue());
+      }
+      return processor;
+    }
+    catch (final ClassNotFoundException e)
+    {
+      LOGGER.error("Could not find processor class", e);
+      throw new ProcessorException(e);
+    }
+    catch (final NoSuchMethodException e)
+    {
+      LOGGER.error("Could not find constructor in processor class", e);
+      throw new ProcessorException(e);
+    }
+    catch (final InvocationTargetException e)
+    {
+      LOGGER.error("Processor constructor failed", e);
+      throw new ProcessorException(e);
+    }
+    catch (final IllegalAccessException e)
+    {
+      LOGGER.error("Processor constructor not accessible", e);
+      throw new ProcessorException(e);
+    }
+    catch (final InstantiationException e)
+    {
+      LOGGER.error("Could not instantiate processor", e);
+      throw new ProcessorException(e);
+    }
   }
 
 }
--- config-processor/src/main/java/com/baxter/config/processor/desc/package-info.java
+++ config-processor/src/main/java/com/baxter/config/processor/desc/package-info.java
@@ -1,10 +1,9 @@
 /**
  * The Configuration Processors Model package.
  */
-@XmlSchema(namespace = "http://baxter-it.com/config/descriptor", xmlns = { @XmlNs(prefix = "cpd", namespaceURI = "http://baxter-it.com/config/descriptor") }, elementFormDefault=XmlNsForm.QUALIFIED)
+@XmlSchema(namespace = "http://baxter-it.com/config/descriptor", elementFormDefault = XmlNsForm.QUALIFIED)
 package com.baxter.config.processor.desc;
 
-import javax.xml.bind.annotation.XmlSchema;
-import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
 
