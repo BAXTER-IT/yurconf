11b9fc4fea6111e46bc39d5f8080a364263eecdf
Istvan Szabo <maci@baxter-it.com>
2013-09-17 17:01:56 +0200

    Removed SLF4J Dependency

--- config-client/src/main/java/com/baxter/config/client/Request.java
+++ config-client/src/main/java/com/baxter/config/client/Request.java
@@ -12,9 +12,6 @@ import java.util.List;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.baxter.config.om.ConfigID;
 import com.baxter.config.om.ConfigParameter;
 import com.baxter.config.om.Version;
@@ -28,72 +25,73 @@ import com.baxter.config.om.Version;
 public class Request
 {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Request.class);
-
   private final URL url;
 
-  Request(final URL restUrl, final ConfigID configId, final Version version, final List<ConfigParameter> parameters)
-	  throws MalformedURLException
+  Request(final URL restUrl, final ConfigID configId, final Version version,
+      final List<ConfigParameter> parameters) throws MalformedURLException
   {
-	final ConfigParameter[] params = parameters.toArray(new ConfigParameter[parameters.size()]);
-	this.url = buildRequestURL(restUrl, configId, version, params);
+    final ConfigParameter[] params = parameters
+        .toArray(new ConfigParameter[parameters.size()]);
+    url = buildRequestURL(restUrl, configId, version, params);
   }
 
-  private static URL buildRequestURL(final URL restUrl, final ConfigID configId, final Version version,
-	  final ConfigParameter[] parameters) throws MalformedURLException
+  private static URL buildRequestURL(final URL restUrl,
+      final ConfigID configId, final Version version,
+      final ConfigParameter[] parameters) throws MalformedURLException
   {
-	final StringBuilder path = new StringBuilder(restUrl.toString());
-	if (path.charAt(path.length() - 1) == '/')
-	{
-	  path.deleteCharAt(path.length() - 1);
-	}
-	path.append(configId.toURLPath());
-	if (version != null)
-	{
-	  path.append("?");
-	  path.append("version").append("=").append(version);
-	}
-	if (parameters.length != 0)
-	{
-	  path.append(version == null ? "?" : "&");
-	  for (int i = 0; i < parameters.length; i++)
-	  {
-		if (i != 0)
-		{
-		  path.append("&");
-		}
-		path.append(parameters[i].getName());
-		path.append("=");
-		try
-		{
-		  path.append(URLEncoder.encode(parameters[i].getValue(), "UTF-8"));
-		}
-		catch (final UnsupportedEncodingException e)
-		{
-		  throw new IllegalStateException("Cannot encode parameter value for URL", e);
-		}
-	  }
-	}
-	return new URL(path.toString());
+    final StringBuilder path = new StringBuilder(restUrl.toString());
+    if (path.charAt(path.length() - 1) == '/')
+    {
+      path.deleteCharAt(path.length() - 1);
+    }
+    path.append(configId.toURLPath());
+    if (version != null)
+    {
+      path.append("?");
+      path.append("version").append("=").append(version);
+    }
+    if (parameters.length != 0)
+    {
+      path.append(version == null ? "?" : "&");
+      for (int i = 0; i < parameters.length; i++)
+      {
+        if (i != 0)
+        {
+          path.append("&");
+        }
+        path.append(parameters[i].getName());
+        path.append("=");
+        try
+        {
+          path.append(URLEncoder.encode(parameters[i].getValue(), "UTF-8"));
+        }
+        catch (final UnsupportedEncodingException e)
+        {
+          throw new IllegalStateException(
+              "Cannot encode parameter value for URL", e);
+        }
+      }
+    }
+    return new URL(path.toString());
   }
 
   public URL getUrl()
   {
-	return this.url;
+    return url;
   }
 
-  public <T> T loadObject(final Class<T> objectClass) throws ConfigurationException
+  public <T> T loadObject(final Class<T> objectClass)
+      throws ConfigurationException
   {
-	try
-	{
-	  final JAXBContext ctx = JAXBContext.newInstance(objectClass);
-	  final Unmarshaller unmarshaller = ctx.createUnmarshaller();
-	  return objectClass.cast(unmarshaller.unmarshal(getUrl()));
-	}
-	catch (final Exception e)
-	{
-	  LOGGER.error("Failed to get configuration", e);
-	  throw new ConfigurationException("Could not read config", e);
-	}
+    try
+    {
+      final JAXBContext ctx = JAXBContext.newInstance(objectClass);
+      final Unmarshaller unmarshaller = ctx.createUnmarshaller();
+      return objectClass.cast(unmarshaller.unmarshal(getUrl()));
+    }
+    catch (final Exception e)
+    {
+      throw new ConfigurationException("Could not read config", e);
+    }
   }
 }
--- config-om/src/main/java/com/baxter/config/om/jvm/Classpath.java
+++ config-om/src/main/java/com/baxter/config/om/jvm/Classpath.java
@@ -10,41 +10,36 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @author ykryshchuk
- *
+ * 
  */
 @XmlRootElement(name = "classpath")
 public class Classpath
 {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(Classpath.class);
 
   @XmlElement(name = "path")
   private final List<Path> paths = new ArrayList<Path>();
 
   public boolean isEmpty()
   {
-	return paths.isEmpty();
+    return paths.isEmpty();
   }
 
   void add(final Path path)
   {
-	paths.add(path);
+    paths.add(path);
   }
 
   public URL[] getUrls()
   {
-	final URL[] urls = new URL[paths.size()];
-	int i = 0;
-	for (final Path p : paths)
-	{
-	  urls[i++] = p.getUrl();
-	}
-	return urls;
+    final URL[] urls = new URL[paths.size()];
+    int i = 0;
+    for (final Path p : paths)
+    {
+      urls[i++] = p.getUrl();
+    }
+    return urls;
   }
 
 }
