9a9fafd3d3e03c61adb0f76d93fc0361e85eb6cb
goszi <szilard@baxter-it.com>
2013-11-06 14:38:53 +0100

    restURL check added.

--- config-client/src/main/java/com/baxter/config/client/Request.java
+++ config-client/src/main/java/com/baxter/config/client/Request.java
@@ -27,71 +27,72 @@ public class Request
 
   private final URL url;
 
-  Request(final URL restUrl, final ConfigID configId, final Version version,
-      final List<ConfigParameter> parameters) throws MalformedURLException
+  Request(final URL restUrl, final ConfigID configId, final Version version, final List<ConfigParameter> parameters)
+	  throws MalformedURLException
   {
-    final ConfigParameter[] params = parameters
-        .toArray(new ConfigParameter[parameters.size()]);
-    url = buildRequestURL(restUrl, configId, version, params);
+	final ConfigParameter[] params = parameters.toArray(new ConfigParameter[parameters.size()]);
+	url = buildRequestURL(restUrl, configId, version, params);
   }
 
-  private static URL buildRequestURL(final URL restUrl,
-      final ConfigID configId, final Version version,
-      final ConfigParameter[] parameters) throws MalformedURLException
+  private static URL buildRequestURL(final URL restUrl, final ConfigID configId, final Version version,
+	  final ConfigParameter[] parameters) throws MalformedURLException
   {
-    final StringBuilder path = new StringBuilder(restUrl.toString());
-    if (path.charAt(path.length() - 1) == '/')
-    {
-      path.deleteCharAt(path.length() - 1);
-    }
-    path.append(configId.toURLPath());
-    if (version != null)
-    {
-      path.append("?");
-      path.append("version").append("=").append(version);
-    }
-    if (parameters.length != 0)
-    {
-      path.append(version == null ? "?" : "&");
-      for (int i = 0; i < parameters.length; i++)
-      {
-        if (i != 0)
-        {
-          path.append("&");
-        }
-        path.append(parameters[i].getName());
-        path.append("=");
-        try
-        {
-          path.append(URLEncoder.encode(parameters[i].getValue(), "UTF-8"));
-        }
-        catch (final UnsupportedEncodingException e)
-        {
-          throw new IllegalStateException(
-              "Cannot encode parameter value for URL", e);
-        }
-      }
-    }
-    return new URL(path.toString());
+	if (restUrl == null)
+	{
+	  throw new IllegalArgumentException("restUrl is not set");
+	}
+
+	final StringBuilder path = new StringBuilder(restUrl.toString());
+	if (path.charAt(path.length() - 1) == '/')
+	{
+	  path.deleteCharAt(path.length() - 1);
+	}
+	path.append(configId.toURLPath());
+	if (version != null)
+	{
+	  path.append("?");
+	  path.append("version").append("=").append(version);
+	}
+	if (parameters.length != 0)
+	{
+	  path.append(version == null ? "?" : "&");
+	  for (int i = 0; i < parameters.length; i++)
+	  {
+		if (i != 0)
+		{
+		  path.append("&");
+		}
+		path.append(parameters[i].getName());
+		path.append("=");
+		try
+		{
+		  path.append(URLEncoder.encode(parameters[i].getValue(), "UTF-8"));
+		}
+		catch (final UnsupportedEncodingException e)
+		{
+		  throw new IllegalStateException("Cannot encode parameter value for URL", e);
+		}
+	  }
+	}
+	return new URL(path.toString());
   }
 
   public URL getUrl()
   {
-    return url;
+	return url;
   }
 
-  public <T> T loadObject(final Class<T> objectClass)
-      throws ConfigurationException
+  public <T> T loadObject(final Class<T> objectClass) throws ConfigurationException
   {
-    try
-    {
-      final JAXBContext ctx = JAXBContext.newInstance(objectClass);
-      final Unmarshaller unmarshaller = ctx.createUnmarshaller();
-      return objectClass.cast(unmarshaller.unmarshal(getUrl()));
-    }
-    catch (final Exception e)
-    {
-      throw new ConfigurationException("Could not read config", e);
-    }
+	try
+	{
+	  final JAXBContext ctx = JAXBContext.newInstance(objectClass);
+	  final Unmarshaller unmarshaller = ctx.createUnmarshaller();
+	  return objectClass.cast(unmarshaller.unmarshal(getUrl()));
+	}
+	catch (final Exception e)
+	{
+	  throw new ConfigurationException("Could not read config", e);
+	}
   }
 }
