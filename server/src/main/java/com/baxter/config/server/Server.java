55659a83462ca254ee0396a0eefb3bcc16914151
Yuriy Kryshchuk <yuriy.kryshchuk@gmail.com>
2013-12-07 23:22:21 +0200

    CFG-49. Completed refactoring for DEB.

--- server/src/main/java/com/baxter/config/server/Server.java
+++ server/src/main/java/com/baxter/config/server/Server.java
@@ -5,13 +5,13 @@ package com.baxter.config.server;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,22 +35,10 @@ public class Server extends org.eclipse.jetty.server.Server
 
   private static final String DEFAULT_JETTY_HOST = "0.0.0.0";
 
-  private static final int HTTP_IDLE_TIMEOUT = 30000;
-
   private final List<URL> jarUrls = new ArrayList<>();
 
   Server() {
-	final String jettyPortString = System.getProperty("jetty.port");
-	LOGGER.debug("Jetty port value {}", jettyPortString);
-	final int jettyPort = jettyPortString == null ? DEFAULT_JETTY_PORT : Integer.parseInt(jettyPortString);
-	final String jettyHost = System.getProperty("jetty.host", DEFAULT_JETTY_HOST);
-	LOGGER.info("Jetty will run on {}:{}", jettyHost, jettyPort);
-
-	final ServerConnector http = new ServerConnector(this);
-	http.setPort(jettyPort);
-	http.setHost(jettyHost);
-	http.setIdleTimeout(HTTP_IDLE_TIMEOUT);
-	addConnector(http);
+	super(getServerSocketAddres());
 
 	ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 	context.setContextPath("/config-server");
@@ -59,6 +47,15 @@ public class Server extends org.eclipse.jetty.server.Server
 	context.addEventListener(new ProcessorFactoryInitializer());
 	context.setClassLoader(getProcessorsClassLoader());
 	setHandler(context);
+  }
+
+  private static InetSocketAddress getServerSocketAddres() {
+	final String jettyPortString = System.getProperty("jetty.port");
+	LOGGER.debug("Jetty port value {}", jettyPortString);
+	final int jettyPort = jettyPortString == null ? DEFAULT_JETTY_PORT : Integer.parseInt(jettyPortString);
+	final String jettyHost = System.getProperty("jetty.host", DEFAULT_JETTY_HOST);
+	LOGGER.info("Jetty will run on {}:{}", jettyHost, jettyPort);
+	return new InetSocketAddress(jettyHost, jettyPort);
   }
 
   private ClassLoader getProcessorsClassLoader()
