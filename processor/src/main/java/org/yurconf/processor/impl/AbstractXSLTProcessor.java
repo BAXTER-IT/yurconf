a68d37c6ff2f4c83cf87cfe34dc91bd858c0e6e9
Yuriy Kryshchuk <yuriy.kryshchuk@gmail.com>
2014-09-22 20:16:40 +0300

    Javadoc fixed

--- processor/src/main/java/org/yurconf/processor/impl/AbstractXSLTProcessor.java
+++ processor/src/main/java/org/yurconf/processor/impl/AbstractXSLTProcessor.java
@@ -1,10 +1,10 @@
 /*
  * Yurconf Processor Fundamental
  * This software is distributed as is.
- *
+ * 
  * We do not care about any damages that could be caused
  * by this software directly or indirectly.
- *
+ * 
  * Join our team to help make it better.
  */
 package org.yurconf.processor.impl;
